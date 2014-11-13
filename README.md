41c3949a026c806ccf8fa2ad35c4f58a062b700c
Michael Len <michael.len@ooyala.com>
2014-11-12 20:54:29 -0800

    [PBA-977] Added placeholder release scripts and a readme

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a repository of sample applications for the Ooyala SDK for Android.
---
+++ script/pull_latest_release.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Download the latest packages from Box
+# Archive them into vendor/ folder
+# Distribute jar files across all sample apps
