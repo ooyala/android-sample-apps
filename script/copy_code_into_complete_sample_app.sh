809da5de1569f25db214b73127cfe4d0f7be7f9f
Zhihui Chen <zchen@ooyala.com>
2014-12-02 15:18:37 -0800

    [PBA-991] fix script to copy over layout as well.

--- script/copy_code_into_complete_sample_app.sh
+++ script/copy_code_into_complete_sample_app.sh
@@ -7,11 +7,14 @@ cd ${BASE_DIR}
 echo "Copying AdvancedPlaybackSampleApp into CompleteSampleApp"
 cp AdvancedPlaybackSampleApp/src/com/ooyala/sample/players/* CompleteSampleApp/src/com/ooyala/sample/players/
 cp AdvancedPlaybackSampleApp/src/com/ooyala/sample/lists/* CompleteSampleApp/src/com/ooyala/sample/lists/
+cp AdvancedPlaybackSampleApp/res/layout/* CompleteSampleApp/res/layout/
 
 echo "Copying FreewheelSampleApp into CompleteSampleApp"
 cp FreewheelSampleApp/src/com/ooyala/sample/players/* CompleteSampleApp/src/com/ooyala/sample/players/
 cp FreewheelSampleApp/src/com/ooyala/sample/lists/* CompleteSampleApp/src/com/ooyala/sample/lists/
+cp FreewheelSampleApp/res/layout/* CompleteSampleApp/res/layout/
 
 echo "Copying IMASampleApp into CompleteSampleApp"
 cp IMASampleApp/src/com/ooyala/sample/players/* CompleteSampleApp/src/com/ooyala/sample/players/
 cp IMASampleApp/src/com/ooyala/sample/lists/* CompleteSampleApp/src/com/ooyala/sample/lists/
+cp IMASampleApp/res/layout/* CompleteSampleApp/res/layout/
