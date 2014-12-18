77ea5cac10b6597de6915d32106eeb3f359e7e28
Zhihui Chen <zchen@ooyala.com>
2014-12-18 12:37:14 -0800

    [PBA-1092] preload and promoimage sample app.

---
+++ OptionsSampleApp/src/com/ooyala/sample/players/PreloadWithInitTimePlayerActivity.java
@@ -0,0 +1,128 @@
+package com.ooyala.sample.players;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ToggleButton;
+
+import com.ooyala.android.DebugMode;
+import com.ooyala.android.LocalizationSupport;
+import com.ooyala.android.OoyalaPlayer;
+import com.ooyala.android.OoyalaPlayerLayout;
+import com.ooyala.android.PlayerDomain;
+import com.ooyala.android.configuration.Options;
+import com.ooyala.android.ui.OptimizedOoyalaPlayerLayoutController;
+import com.ooyala.sample.R;
+
+public class PreloadWithInitTimePlayerActivity extends Activity implements OnClickListener, Observer {
+  /**
+  * Called when the activity is first created.
+  */
+  private final String TAG = this.getClass().toString();
+  private final String PCODE = "R2d3I6s06RyB712DN0_2GsQS-R-Y";
+  private final String DOMAIN = "http://ooyala.com";
+  private String EMBEDCODE = "NqcGg4bzoOmMiV35ZttQDtBX1oNQBnT-";
+
+  private OptimizedOoyalaPlayerLayoutController playerLayoutController;
+  private OoyalaPlayer player;
+  private Button setButton;
+  private ToggleButton preloadButton;
+  private ToggleButton showPromoImageButton;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+	String localeString = getResources().getConfiguration().locale.toString();
+	Log.d(TAG, "locale is " + localeString);
+	LocalizationSupport.useLocalizedStrings(LocalizationSupport
+	    .loadLocalizedStrings(localeString));
+	
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.player_toggle_button_layout);
+	EMBEDCODE = getIntent().getExtras().getString("embed_code");
+	
+	setButton = (Button) findViewById(R.id.setButton);
+	setButton.setText("Create Video");
+	setButton.setOnClickListener(this);
+	
+	preloadButton = (ToggleButton) findViewById(R.id.toggleButton1);
+	preloadButton.setTextOn("Preload On");
+	preloadButton.setTextOff("Preload Off");
+	preloadButton.setChecked(true);
+	
+	showPromoImageButton = (ToggleButton) findViewById(R.id.toggleButton2);
+	showPromoImageButton.setTextOn("Show PromoImage On");
+	showPromoImageButton.setTextOff("Show PromoImage Off");
+	showPromoImageButton.setChecked(true);
+  }
+
+  @Override
+  protected void onStop() {
+	super.onStop();
+	Log.d(TAG, "App Stopped");
+	if (playerLayoutController.getPlayer() != null) {
+	  playerLayoutController.getPlayer().suspend();
+	}
+  }
+	
+  @Override
+  protected void onRestart() {
+	super.onRestart();
+	Log.d(TAG, "App Restarted");
+	if (playerLayoutController.getPlayer() != null) {
+	  playerLayoutController.getPlayer().resume();
+	}
+  }
+	
+  @Override
+  public void onClick(View v) {
+	OoyalaPlayerLayout playerLayout = (OoyalaPlayerLayout) findViewById(R.id.ooyalaPlayer);
+	PlayerDomain domain = new PlayerDomain(DOMAIN);
+	boolean showPromoImage = this.showPromoImageButton.isChecked();
+	boolean preload = this.preloadButton.isChecked();
+	DebugMode.logD(TAG, "showPromoImage: " + showPromoImage
+	    + " preload: " + preload);
+	Options options = new Options.Builder().setPreloadContent(preload).setShowPromoImage(showPromoImage).build();
+	playerLayoutController = new OptimizedOoyalaPlayerLayoutController(
+	    playerLayout, PCODE, domain, options);
+	player = playerLayoutController.getPlayer();
+	player.addObserver(this);
+	
+	player.setEmbedCode(EMBEDCODE);
+	player.play(20000);
+  }
+		
+  @Override
+  public boolean onKeyUp(int keyCode, KeyEvent event) {
+	if (playerLayoutController.onKeyUp(keyCode, event)) {
+	  return true;
+	}
+	return super.onKeyDown(keyCode, event);
+  }
+	
+  @Override
+  public void update(Observable arg0, Object arg1) {
+	if (arg1 == OoyalaPlayer.TIME_CHANGED_NOTIFICATION) {
+	  return;
+	}
+	Log.d(TAG,
+	    "Notification Recieved: " + arg1 + " - state: " + player.getState());
+  }
+	
+  public void onToggleClicked(View view) {
+	// Is the toggle on?
+	boolean on = ((ToggleButton) view).isChecked();
+	    
+	if (on) {
+	  // do something
+	} else {
+	  // do something
+	}
+  }
+}
