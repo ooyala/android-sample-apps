f17a86e47f54edfd9c111eb5d34ae4fd601a7f34
Matthew Chen <zchen@ooyala.com>
2016-01-11 16:30:26 -0800

    [PBA-3045] port android skin sample app to android sample app repo.

---
+++ OoyalaSkinSampleApp/app/src/main/java/com/ooyala/sample/players/BasicPlaybackVideoPlayerActivity.java
@@ -0,0 +1,112 @@
+package com.ooyala.sample.players;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import com.ooyala.android.OoyalaPlayer;
+import com.ooyala.android.PlayerDomain;
+import com.ooyala.android.configuration.Options;
+import com.ooyala.android.ooyalaskinsampleapp.R;
+import com.ooyala.android.ooyalaskinsdk.OoyalaSkinLayout;
+import com.ooyala.android.ooyalaskinsdk.OoyalaSkinLayoutController;
+
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * This activity illustrates how you can play basicPlayback Video
+ * you can also play Ooyala and VAST advertisements programmatically
+ * through the SDK
+ *
+ */
+public class BasicPlaybackVideoPlayerActivity extends Activity implements Observer {
+  final String TAG = this.getClass().toString();
+
+  String EMBED = null;
+  final String PCODE  = "R2d3I6s06RyB712DN0_2GsQS-R-Y";
+  final String DOMAIN = "http://ooyala.com";
+
+  protected OoyalaSkinLayoutController playerLayoutController;
+  protected OoyalaPlayer player;
+
+  /**
+   * Called when the activity is first created.
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setTitle(getIntent().getExtras().getString("selection_name"));
+    setContentView(R.layout.player_simple_layout);
+    EMBED = getIntent().getExtras().getString("embed_code");
+
+    //Initialize the player
+//    OoyalaPlayerLayout playerLayout = (OoyalaPlayerLayout) findViewById(R.id.ooyalaSkin);
+//    player = new OoyalaPlayer(PCODE, new PlayerDomain(DOMAIN));
+//    playerLayoutController = new OoyalaPlayerLayoutController(playerLayout, player);
+//    player.addObserver(this);
+
+    OoyalaSkinLayout skinLayout = (OoyalaSkinLayout)findViewById(R.id.ooyalaSkin);
+    PlayerDomain domain = new PlayerDomain(DOMAIN);
+    Options options = new Options.Builder().setShowAdsControls(false)
+            .setShowCuePoints(true).setShowPromoImage(true)
+            .setPreloadContent(false).build();
+    player = new OoyalaPlayer(PCODE, domain, options);
+    skinLayout.setupViews(getApplication(), player);
+//    playerLayoutController = new OoyalaSkinLayoutController(context,skinLayout, player);
+//    player.addObserver(this);
+
+
+    if (player.setEmbedCode(EMBED)) {
+      //player.play();
+    }
+    else {
+      Log.e(TAG, "Asset Failure");
+    }
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    Log.d(TAG, "Player Activity Stopped");
+    if (player != null) {
+      player.suspend();
+    }
+  }
+
+  @Override
+  protected void onRestart() {
+    super.onRestart();
+    Log.d(TAG, "Player Activity Restarted");
+    if (player != null) {
+      player.resume();
+    }
+  }
+
+  /**
+   * Listen to all notifications from the OoyalaPlayer
+   */
+  @Override
+  public void update(Observable arg0, Object arg1) {
+    if (arg0 != player) {
+      return;
+    }
+
+    if (arg1 == OoyalaPlayer.TIME_CHANGED_NOTIFICATION) {
+      return;
+    }
+
+    if (arg1 == OoyalaPlayer.ERROR_NOTIFICATION) {
+      final String msg = "Error Event Received";
+      if (player != null && player.getError() != null) {
+        Log.e(TAG, msg, player.getError());
+      }
+      else {
+        Log.e(TAG, msg);
+      }
+      return;
+    }
+
+    Log.d(TAG, "Notification Received: " + arg1 + " - state: " + player.getState());
+  }
+
+}
