973ea166c4bee028ef6ea9de19c9e17734b517e0
Matthew Chen <zchen@ooyala.com>
2016-06-09 13:58:46 -0700

    [dev] merge android complete app

--- CompleteSampleApp/app/src/main/java/com/ooyala/sample/players/PreventVideoViewSharingPlayerActivity.java
+++ CompleteSampleApp/app/src/main/java/com/ooyala/sample/players/PreventVideoViewSharingPlayerActivity.java
@@ -11,6 +11,7 @@ import com.ooyala.android.PlayerDomain;
 import com.ooyala.android.configuration.Options;
 import com.ooyala.android.ui.OoyalaPlayerLayoutController;
 import com.ooyala.sample.R;
+import com.ooyala.android.util.SDCardLogcatOoyalaEventsLogger;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -18,88 +19,94 @@ import java.util.Observer;
 /**
  * This activity illustrates how you can set PreventVideoViewSharing such that third party
  * applications cannot screen-capture video
- *
  */
 public class PreventVideoViewSharingPlayerActivity extends Activity implements Observer {
-  final String TAG = this.getClass().toString();
+    final String TAG = this.getClass().toString();
 
-  String EMBED = null;
-  final String PCODE  = "R2d3I6s06RyB712DN0_2GsQS-R-Y";
-  final String DOMAIN = "http://ooyala.com";
+    String EMBED = null;
+    final String PCODE = "R2d3I6s06RyB712DN0_2GsQS-R-Y";
+    final String DOMAIN = "http://ooyala.com";
 
-  protected OoyalaPlayerLayoutController playerLayoutController;
-  protected OoyalaPlayer player;
+    protected OoyalaPlayerLayoutController playerLayoutController;
+    protected OoyalaPlayer player;
+    SDCardLogcatOoyalaEventsLogger playbacklog;
 
-  /**
-   * Called when the activity is first created.
-   */
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setTitle(getIntent().getExtras().getString("selection_name"));
-    setContentView(R.layout.player_simple_layout);
-    EMBED = getIntent().getExtras().getString("embed_code");
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(getIntent().getExtras().getString("selection_name"));
+        setContentView(R.layout.player_simple_layout);
+        EMBED = getIntent().getExtras().getString("embed_code");
 
-    Options options = new Options.Builder().setPreventVideoViewSharing(true).build();
+        // Initialize playBackLog : Write the sdk events text along with events count to log file in sdcard if the log file already exists
+        playbacklog = new SDCardLogcatOoyalaEventsLogger();
 
-    //Initialize the player
-    OoyalaPlayerLayout playerLayout = (OoyalaPlayerLayout) findViewById(R.id.ooyalaPlayer);
-    player = new OoyalaPlayer(PCODE, new PlayerDomain(DOMAIN), options);
-    playerLayoutController = new OoyalaPlayerLayoutController(playerLayout, player);
-    player.addObserver(this);
+        Options options = new Options.Builder().setPreventVideoViewSharing(true).build();
 
-    if (player.setEmbedCode(EMBED)) {
-      player.play();
-    }
-    else {
-      Log.e(TAG, "Asset Failure");
-    }
-  }
+        //Initialize the player
+        OoyalaPlayerLayout playerLayout = (OoyalaPlayerLayout) findViewById(R.id.ooyalaPlayer);
+        player = new OoyalaPlayer(PCODE, new PlayerDomain(DOMAIN), options);
+        playerLayoutController = new OoyalaPlayerLayoutController(playerLayout, player);
+        player.addObserver(this);
 
-  @Override
-  protected void onStop() {
-    super.onStop();
-    Log.d(TAG, "Player Activity Stopped");
-    if (player != null) {
-      player.suspend();
-    }
-  }
-
-  @Override
-  protected void onRestart() {
-    super.onRestart();
-    Log.d(TAG, "Player Activity Restarted");
-    if (player != null) {
-      player.resume();
-    }
-  }
-
-  /**
-   * Listen to all notifications from the OoyalaPlayer
-   */
-  @Override
-  public void update(Observable arg0, Object argN) {
-    if (arg0 != player) {
-      return;
+        if (player.setEmbedCode(EMBED)) {
+            player.play();
+        } else {
+            Log.e(TAG, "Asset Failure");
+        }
     }
 
-    final String arg1 = OoyalaNotification.getNameOrUnknown(argN);
-    if (arg1 == OoyalaPlayer.TIME_CHANGED_NOTIFICATION_NAME) {
-      return;
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "Player Activity Stopped");
+        if (player != null) {
+            player.suspend();
+        }
     }
 
-    if (arg1 == OoyalaPlayer.ERROR_NOTIFICATION_NAME) {
-      final String msg = "Error Event Received";
-      if (player != null && player.getError() != null) {
-        Log.e(TAG, msg, player.getError());
-      }
-      else {
-        Log.e(TAG, msg);
-      }
-      return;
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "Player Activity Restarted");
+        if (player != null) {
+            player.resume();
+        }
     }
 
-    Log.d(TAG, "Notification Received: " + arg1 + " - state: " + player.getState());
-  }
+    /**
+     * Listen to all notifications from the OoyalaPlayer
+     */
+    @Override
+    public void update(Observable arg0, Object argN) {
+        if (arg0 != player) {
+            return;
+        }
+
+        final String arg1 = OoyalaNotification.getNameOrUnknown(argN);
+        if (arg1 == OoyalaPlayer.TIME_CHANGED_NOTIFICATION_NAME) {
+            return;
+        }
+
+        if (arg1 == OoyalaPlayer.ERROR_NOTIFICATION_NAME) {
+            final String msg = "Error Event Received";
+            if (player != null && player.getError() != null) {
+                Log.e(TAG, msg, player.getError());
+            } else {
+                Log.e(TAG, msg);
+            }
+            return;
+        }
+
+        // Automation Hook: to write Notifications to a temporary file on the device/emulator
+        String text = "Notification Received: " + arg1 + " - state: " + player.getState();
+        // Automation Hook: Write the event text along with event count to log file in sdcard if the log file exists
+        playbacklog.writeToSdcardLog(text);
+
+        Log.d(TAG, text);
+    }
 
 }
--- CompleteSampleApp/app/src/main/java/com/ooyala/sample/players/ServerConfiguredTVRatingsPlayerActivity.java
+++ CompleteSampleApp/app/src/main/java/com/ooyala/sample/players/ServerConfiguredTVRatingsPlayerActivity.java
@@ -16,74 +16,83 @@ import com.ooyala.android.configuration.FCCTVRatingConfiguration.Position;
 import com.ooyala.android.configuration.Options;
 import com.ooyala.android.ui.OptimizedOoyalaPlayerLayoutController;
 import com.ooyala.sample.R;
+import com.ooyala.android.util.SDCardLogcatOoyalaEventsLogger;
 
 /**
  * This activity illustrates how you enable TV Ratings Display.
- *
+ * <p/>
  * In order for this to work, you must have the following values in your asset's CustomMetadata:
- *   tvrating
- *   tvratingsurl (optional)
- *   tvsubratings (optional)
+ * tvrating
+ * tvratingsurl (optional)
+ * tvsubratings (optional)
  */
 public class ServerConfiguredTVRatingsPlayerActivity extends Activity implements Observer {
-  /**
-  * Called when the activity is first created.
-  */
-  private final String TAG = this.getClass().toString();
-  private final String PCODE = "R2d3I6s06RyB712DN0_2GsQS-R-Y";
-  private final String DOMAIN = "http://ooyala.com";
-  private String EMBEDCODE = "";
+    /**
+     * Called when the activity is first created.
+     */
+    private final String TAG = this.getClass().toString();
+    private final String PCODE = "R2d3I6s06RyB712DN0_2GsQS-R-Y";
+    private final String DOMAIN = "http://ooyala.com";
+    private String EMBEDCODE = "";
 
-  private OptimizedOoyalaPlayerLayoutController playerLayoutController;
-  private OoyalaPlayer player;
+    private OptimizedOoyalaPlayerLayoutController playerLayoutController;
+    private OoyalaPlayer player;
+    SDCardLogcatOoyalaEventsLogger playbacklog;
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.player_simple_frame_layout);
-	EMBEDCODE = getIntent().getExtras().getString("embed_code");
-	
-	OoyalaPlayerLayout playerLayout = (OoyalaPlayerLayout) findViewById(R.id.ooyalaPlayer);
-	PlayerDomain domain = new PlayerDomain(DOMAIN);
-	
-	// Configure FCC TV Ratings
-	FCCTVRatingConfiguration.Builder builder = new FCCTVRatingConfiguration.Builder();
-	FCCTVRatingConfiguration tvRatingConfiguration = builder.setPosition(Position.TopLeft).setDurationSeconds(5).build();
-	Options options = new Options.Builder().setTVRatingConfiguration(tvRatingConfiguration).build();
-	
-    player = new OoyalaPlayer(PCODE, domain, options);
-    playerLayoutController = new OptimizedOoyalaPlayerLayoutController(playerLayout, player);
-	player.addObserver(this);
-	
-	player.setEmbedCode(EMBEDCODE);
-	player.play();
-  }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.player_simple_frame_layout);
+        EMBEDCODE = getIntent().getExtras().getString("embed_code");
 
-  @Override
-  protected void onStop() {
-	super.onStop();
-	Log.d(TAG, "App Stopped");
-	if (playerLayoutController.getPlayer() != null) {
-	  playerLayoutController.getPlayer().suspend();
-	}
-  }
-	
-  @Override
-  protected void onRestart() {
-	super.onRestart();
-	Log.d(TAG, "App Restarted");
-	if (playerLayoutController.getPlayer() != null) {
-	  playerLayoutController.getPlayer().resume();
-	}
-  }
+        // Initialize playBackLog : Write the sdk events text along with events count to log file in sdcard if the log file already exists
+        playbacklog = new SDCardLogcatOoyalaEventsLogger();
 
-  @Override
-  public void update(Observable arg0, Object argN) {
-  final String arg1 = OoyalaNotification.getNameOrUnknown(argN);
-	if (arg1 == OoyalaPlayer.TIME_CHANGED_NOTIFICATION_NAME) {
-	  return;
-	}
-	Log.d(TAG,
-	    "Notification Received: " + arg1 + " - state: " + player.getState());
-  }
+        OoyalaPlayerLayout playerLayout = (OoyalaPlayerLayout) findViewById(R.id.ooyalaPlayer);
+        PlayerDomain domain = new PlayerDomain(DOMAIN);
+
+        // Configure FCC TV Ratings
+        FCCTVRatingConfiguration.Builder builder = new FCCTVRatingConfiguration.Builder();
+        FCCTVRatingConfiguration tvRatingConfiguration = builder.setPosition(Position.TopLeft).setDurationSeconds(5).build();
+        Options options = new Options.Builder().setTVRatingConfiguration(tvRatingConfiguration).build();
+
+        player = new OoyalaPlayer(PCODE, domain, options);
+        playerLayoutController = new OptimizedOoyalaPlayerLayoutController(playerLayout, player);
+        player.addObserver(this);
+
+        player.setEmbedCode(EMBEDCODE);
+        player.play();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "App Stopped");
+        if (playerLayoutController.getPlayer() != null) {
+            playerLayoutController.getPlayer().suspend();
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "App Restarted");
+        if (playerLayoutController.getPlayer() != null) {
+            playerLayoutController.getPlayer().resume();
+        }
+    }
+
+    @Override
+    public void update(Observable arg0, Object argN) {
+        final String arg1 = OoyalaNotification.getNameOrUnknown(argN);
+        if (arg1 == OoyalaPlayer.TIME_CHANGED_NOTIFICATION_NAME) {
+            return;
+        }
+
+        // Automation Hook: to write Notifications to a temporary file on the device/emulator
+        String text = "Notification Received: " + arg1 + " - state: " + player.getState();
+        // Automation Hook: Write the event text along with event count to log file in sdcard if the log file exists
+        playbacklog.writeToSdcardLog(text);
+        Log.d(TAG, text);
+    }
 }
