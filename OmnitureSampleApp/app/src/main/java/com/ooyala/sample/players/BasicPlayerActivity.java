7449187e78418240f10811f5dbff144c776438a2
Unnath Kumar <ukumar@ooyala.com>
2016-08-24 13:40:25 -0700

    omniture to use Skin UI

--- OmnitureSampleApp/app/src/main/java/com/ooyala/sample/players/BasicPlayerActivity.java
+++ OmnitureSampleApp/app/src/main/java/com/ooyala/sample/players/BasicPlayerActivity.java
@@ -3,20 +3,26 @@ package com.ooyala.sample.players;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.ooyala.android.OoyalaNotification;
 import com.ooyala.android.OoyalaPlayer;
 import com.ooyala.android.OoyalaPlayerLayout;
 import com.ooyala.android.PlayerDomain;
 import com.ooyala.android.adobeanalyticssdk.OoyalaAdobeAnalyticsManager;
 import com.ooyala.android.adobeanalyticssdk.OoyalaAdobeHeartbeatConfiguration;
+import com.ooyala.android.configuration.Options;
+import com.ooyala.android.skin.OoyalaSkinLayout;
+import com.ooyala.android.skin.OoyalaSkinLayoutController;
+import com.ooyala.android.skin.configuration.SkinOptions;
 import com.ooyala.android.ui.OoyalaPlayerLayoutController;
 import com.ooyala.sample.R;
 
 import java.util.Observable;
 import java.util.Observer;
 
-public class BasicPlayerActivity extends AppCompatActivity implements Observer {
+public class BasicPlayerActivity extends AppCompatActivity implements Observer,DefaultHardwareBackBtnHandler {
     private static final String TAG = "BasicPlayerActivity";
 
     final String PLAYER_DOMAIN = "http://www.ooyala.com/";
@@ -24,21 +30,30 @@ public class BasicPlayerActivity extends AppCompatActivity implements Observer {
     final String HB_PUBLISHER = "[INSERT YOUR PROVIDER HERE]";
 
     protected OoyalaPlayer player;
-    protected String embedCode;
-    protected String pcode;
+    protected String EMBEDCODE;
+    protected String PCODE;
     protected OoyalaAdobeAnalyticsManager analyticsManager;
+    protected OoyalaSkinLayoutController controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_player);
 
-        embedCode = getIntent().getExtras().getString("embed_code");
-        pcode = getIntent().getExtras().getString("pcode");
+        EMBEDCODE = getIntent().getExtras().getString("embed_code");
+        PCODE = getIntent().getExtras().getString("pcode");
 
-        OoyalaPlayerLayout playerLayout = (OoyalaPlayerLayout) findViewById(R.id.playerLayout);
-        player = new OoyalaPlayer(pcode, new PlayerDomain(PLAYER_DOMAIN));
-        new OoyalaPlayerLayoutController(playerLayout, player);
+        //Initialize the player
+        OoyalaSkinLayout skinLayout = (OoyalaSkinLayout)findViewById(R.id.playerLayout);
+
+        // Create the OoyalaPlayer, with some built-in UI disabled
+        PlayerDomain domain = new PlayerDomain(PLAYER_DOMAIN);
+        Options options = new Options.Builder().setShowPromoImage(false).build();
+        player = new OoyalaPlayer(PCODE, domain, options);
+
+        //Create the SkinOptions, and setup React
+        SkinOptions skinOptions = new SkinOptions.Builder().build();
+        controller = new OoyalaSkinLayoutController(getApplication(), skinLayout, player, skinOptions);
         player.addObserver(this);
 
         OoyalaAdobeHeartbeatConfiguration config = OoyalaAdobeHeartbeatConfiguration.builder()
@@ -49,37 +64,67 @@ public class BasicPlayerActivity extends AppCompatActivity implements Observer {
                 config, getApplicationContext());
         analyticsManager.startCapture();
 
-        if (player.setEmbedCode(embedCode)) {
-            player.play();
+        if (player.setEmbedCode(EMBEDCODE)) {
+            //player.play();
         } else {
             Log.e(TAG, "Player failed when setting embed code.");
+        }
+    }
+    // Observer methods
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        controller.onKeyDown(keyCode, event);
+        return super.onKeyDown(keyCode, event);
+    }
+
+    /** Start DefaultHardwareBackBtnHandler **/
+    @Override
+    public void invokeDefaultOnBackPressed() {
+        super.onBackPressed();
+    }
+/** End DefaultHardwareBackBtnHandler **/
+
+    /** Start Activity methods for Skin **/
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (controller != null) {
+            controller.onPause();
+        }
+        Log.d(TAG, "Player Activity Stopped");
+        if (player != null) {
+            player.suspend();
         }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        if (null != player) {
+        if (controller != null) {
+            controller.onResume( this, this );
+        }
+        Log.d(TAG, "Player Activity Restarted");
+        if (player != null) {
             player.resume();
         }
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        if (null != player) {
-            player.suspend();
+    public void onBackPressed() {
+        if (controller != null) {
+            controller.onBackPressed();
+        } else {
+            super.onBackPressed();
         }
     }
-
     @Override
     protected void onDestroy() {
-        analyticsManager.stopCapture();
-
         super.onDestroy();
+        analyticsManager.stopCapture();
+        if (controller != null) {
+            controller.onDestroy();
+        }
     }
-
-    // Observer methods
 
     @Override
     public void update(Observable observable, Object data) {
