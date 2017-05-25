217b6da0791ec2f7a427fa446b9d95ebeac6e9a0
Shivam Gupta <shivam.gupta@vertisinfotech.com>
2017-05-25 16:28:48 +0530

    modified IMA players activity for allow access of storage permission and also added Abstarct hook activity

--- IMASampleApp/app/src/main/java/com/ooyala/sample/players/CustomConfiguredIMAPlayerActivity.java
+++ IMASampleApp/app/src/main/java/com/ooyala/sample/players/CustomConfiguredIMAPlayerActivity.java
@@ -1,5 +1,7 @@
 package com.ooyala.sample.players;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -25,95 +27,35 @@ import com.ooyala.android.util.SDCardLogcatOoyalaEventsLogger;
  * imaManager.setAdUrlOverride(String)
  * imaManager.setAdTagParameters(Map<String, String>)
  */
-public class CustomConfiguredIMAPlayerActivity extends Activity implements Observer {
-  public final static String getName() {
-    return "Custom Configured IMA Player";
-  }
-  final String TAG = this.getClass().toString();
+public class CustomConfiguredIMAPlayerActivity extends AbstractHookActivity {
+	protected OptimizedOoyalaPlayerLayoutController playerLayoutController;
 
-  String EMBED = null;
-  String PCODE = null;
-  String DOMAIN = null;
+	/**
+	 * Called when the activity is first created.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.player_simple_frame_layout);
+		completePlayerSetup(asked);
 
-  protected OptimizedOoyalaPlayerLayoutController playerLayoutController;
-  protected OoyalaPlayer player;
+	}
+	@Override
+	void completePlayerSetup(final boolean asked) {
+		if (asked) {
+			player = new OoyalaPlayer(pcode, new PlayerDomain(domain));
+			player.addObserver(this);
 
-  SDCardLogcatOoyalaEventsLogger playbacklog;
+			OoyalaPlayerLayout playerLayout = (OoyalaPlayerLayout) findViewById(R.id.ooyalaPlayer);
+			playerLayoutController = new OptimizedOoyalaPlayerLayoutController(playerLayout, player);
 
-  /**
-   * Called when the activity is first created.
-   */
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setTitle(getName());
-    setContentView(R.layout.player_simple_frame_layout);
+			/** DITA_START:<ph id="ima_custom"> **/
+			//OoyalaIMAManager imaManager = new OoyalaIMAManager(player);
 
-    EMBED = getIntent().getExtras().getString("embed_code");
-    PCODE = getIntent().getExtras().getString("pcode");
-    DOMAIN = getIntent().getExtras().getString("domain");
+			/** DITA_END:</ph> **/
 
-    //Initialize the player
-    OoyalaPlayerLayout playerLayout = (OoyalaPlayerLayout) findViewById(R.id.ooyalaPlayer);
-
-    Options options = new Options.Builder().setUseExoPlayer(true).build();
-    player = new OoyalaPlayer(PCODE, new PlayerDomain(DOMAIN), options);
-    playerLayoutController = new OptimizedOoyalaPlayerLayoutController(playerLayout, player);
-    player.addObserver(this);
-
-    // Initialize playBackLog : Write the sdk events text along with events count to log file in sdcard if the log file already exists
-    playbacklog = new SDCardLogcatOoyalaEventsLogger();
-
-    /** DITA_START:<ph id="ima_custom"> **/
-    OoyalaIMAConfiguration imaConfig = new OoyalaIMAConfiguration.Builder().setLocaleOverride("fr").build();
-
-	OoyalaIMAManager imaManager = new OoyalaIMAManager(player, imaConfig );
-	
-	// This ad tag returns a midroll video
-    imaManager.setAdUrlOverride("http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/7521029/pb_test_mid&ciu_szs=640x480&impl=s&cmsid=949&vid=FjbGRjbzp0DV_5-NtXBVo5Rgp3Sj0R5C&gdfp_req=1&env=vp&output=xml_vast2&unviewed_position_start=1&url=[referrer_url]&description_url=[description_url]&correlator=[timestamp]");
-    // imaManager.setAdTagParameters(null);
-    /** DITA_END:</ph> **/
-    
-    if (player.setEmbedCode(EMBED)) {
-      //Uncomment for Auto-Play
-      //player.play();
-    }
-  }
-
-  @Override
-  protected void onStop() {
-    super.onStop();
-    Log.d(TAG, "Player Activity Stopped");
-    if (player != null) {
-      player.suspend();
-    }
-  }
-
-  @Override
-  protected void onRestart() {
-    super.onRestart();
-    Log.d(TAG, "Player Activity Restarted");
-    if (player != null) {
-      player.resume();
-    }
-  }
-
-  /**
-   * Listen to all notifications from the OoyalaPlayer
-   */
-  @Override
-  public void update(Observable arg0, Object argN) {
-    final String arg1 = OoyalaNotification.getNameOrUnknown(argN);
-    if (arg1 == OoyalaPlayer.TIME_CHANGED_NOTIFICATION_NAME) {
-      return;
-    }
-
-    // Automation Hook: to write Notifications to a temporary file on the device/emulator
-    String text="Notification Received: " + arg1 + " - state: " + player.getState();
-    // Automation Hook: Write the event text along with event count to log file in sdcard if the log file exists
-    playbacklog.writeToSdcardLog(text);
-
-    Log.d(TAG, text);
-  }
-
+			player.setEmbedCode(embedCode);
+		}
+	}
 }
+
--- IMASampleApp/app/src/main/java/com/ooyala/sample/players/PreconfiguredIMAPlayerActivity.java
+++ IMASampleApp/app/src/main/java/com/ooyala/sample/players/PreconfiguredIMAPlayerActivity.java
@@ -1,49 +1,31 @@
 package com.ooyala.sample.players;
 
-import java.util.Observable;
-import java.util.Observer;
 
-import android.app.Activity;
+
 import android.os.Bundle;
-import android.util.Log;
-
 import com.ooyala.android.OoyalaPlayer;
-import com.ooyala.android.OoyalaNotification;
 import com.ooyala.android.OoyalaPlayerLayout;
 import com.ooyala.android.PlayerDomain;
-import com.ooyala.android.configuration.Options;
 import com.ooyala.android.imasdk.OoyalaIMAManager;
 import com.ooyala.android.ui.OptimizedOoyalaPlayerLayoutController;
 import com.ooyala.sample.R;
-import com.ooyala.android.util.SDCardLogcatOoyalaEventsLogger;
 
 /**
  * This activity illustrates how to use Freewheel when all configuration is stored in Ooyala Servers
  *
  * In order for Freewheel to work this simply, you need the following parameters set in your Third Party Module Parameters
- * - fw_android_ad_server
- * - fw_android_player_profile
- * 
- * And an Freewheel Ad Spot configured in Backlot with at least the following:
+ * - ima_android_ad_server
+ * - ima_android_player_profile
+ *
+ * And an IMA Ad Spot configured in Backlot with at least the following:
  * - Network ID
  * - Video Asset Network ID
  * - Site Section ID
- * 
+ *
  */
-public class PreconfiguredIMAPlayerActivity extends Activity implements Observer {
-  public final static String getName() {
-    return "Preconfigured IMA Player";
-  }
-  final String TAG = this.getClass().toString();
-
-  String EMBED = null;
-  String PCODE = null;
-  String DOMAIN = null;
+public class PreconfiguredIMAPlayerActivity extends AbstractHookActivity {
 
   protected OptimizedOoyalaPlayerLayoutController playerLayoutController;
-  protected OoyalaPlayer player;
-
-  SDCardLogcatOoyalaEventsLogger playbacklog;
 
   /**
    * Called when the activity is first created.
@@ -51,70 +33,23 @@ public class PreconfiguredIMAPlayerActivity extends Activity implements Observer
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setTitle(getName());
     setContentView(R.layout.player_simple_frame_layout);
-
-    EMBED = getIntent().getExtras().getString("embed_code");
-    PCODE = getIntent().getExtras().getString("pcode");
-    DOMAIN = getIntent().getExtras().getString("domain");
-
-    /** DITA_START:<ph id="ima_preconfigured"> **/
-    //Initialize the player
-    OoyalaPlayerLayout playerLayout = (OoyalaPlayerLayout) findViewById(R.id.ooyalaPlayer);
-
-    Options options = new Options.Builder().setUseExoPlayer(true).build();
-    player = new OoyalaPlayer(PCODE, new PlayerDomain(DOMAIN), options);
-    playerLayoutController = new OptimizedOoyalaPlayerLayoutController(playerLayout, player);
-    player.addObserver(this);
-
-    // Initialize playBackLog : Write the sdk events text along with events count to log file in sdcard if the log file already exists
-    playbacklog = new SDCardLogcatOoyalaEventsLogger();
-
-    @SuppressWarnings("unused")
-	OoyalaIMAManager imaManager = new OoyalaIMAManager(player);
-    
-    if (player.setEmbedCode(EMBED)) {
-      //Uncomment for Auto-Play
-      //player.play();
-    }
-    /** DITA_END:</ph> **/
-
+    completePlayerSetup(asked);
   }
 
   @Override
-  protected void onStop() {
-    super.onStop();
-    Log.d(TAG, "Player Activity Stopped");
-    if (player != null) {
-      player.suspend();
+  void completePlayerSetup(final boolean asked) {
+    if (asked) {
+      player = new OoyalaPlayer(pcode, new PlayerDomain(domain));
+      player.addObserver(this);
+
+      OoyalaPlayerLayout playerLayout = (OoyalaPlayerLayout) findViewById(R.id.ooyalaPlayer);
+      playerLayoutController = new OptimizedOoyalaPlayerLayoutController(playerLayout, player);
+
+      @SuppressWarnings("unused")
+      OoyalaIMAManager imaManager = new OoyalaIMAManager(player);
+
+      player.setEmbedCode(embedCode);
     }
   }
-
-  @Override
-  protected void onRestart() {
-    super.onRestart();
-    Log.d(TAG, "Player Activity Restarted");
-    if (player != null) {
-      player.resume();
-    }
-  }
-
-  /**
-   * Listen to all notifications from the OoyalaPlayer
-   */
-  @Override
-  public void update(Observable arg0, Object argN) {
-    final String arg1 = OoyalaNotification.getNameOrUnknown(argN);
-    if (arg1 == OoyalaPlayer.TIME_CHANGED_NOTIFICATION_NAME) {
-      return;
-    }
-
-    // Automation Hook: to write Notifications to a temporary file on the device/emulator
-    String text="Notification Received: " + arg1 + " - state: " + player.getState();
-    // Automation Hook: Write the event text along with event count to log file in sdcard if the log file exists
-    playbacklog.writeToSdcardLog(text);
-
-    Log.d(TAG, text);
-  }
-
 }
