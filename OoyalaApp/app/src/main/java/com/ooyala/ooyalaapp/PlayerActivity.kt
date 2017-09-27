4109339f14ad0c1d1e5cc39ae31f5c059fcca4cd
Eric Vargas <evargas@ooyala.com>
2017-09-27 14:07:11 -0700

    Start by creating a new sample app

---
+++ OoyalaApp/app/src/main/java/com/ooyala/ooyalaapp/PlayerActivity.kt
@@ -0,0 +1,12 @@
+package com.ooyala.ooyalaapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+    }
+}
