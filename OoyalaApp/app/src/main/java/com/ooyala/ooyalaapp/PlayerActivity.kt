99ffa243cb01e1418c4e930664426439cbba70f0
Eric Vargas <evargas@ooyala.com>
2017-10-09 13:48:56 -0700

    Use SimpleExoPlayerView with ExoStreamPlayer

--- OoyalaApp/app/src/main/java/com/ooyala/ooyalaapp/PlayerActivity.kt
+++ OoyalaApp/app/src/main/java/com/ooyala/ooyalaapp/PlayerActivity.kt
@@ -1,12 +1,25 @@
 package com.ooyala.ooyalaapp
 
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.ooyala.ExoStreamPlayer
+
+import kotlinx.android.synthetic.main.activity_player.*
 
 class PlayerActivity : AppCompatActivity() {
+
+    var ooyalaExoPlayer : ExoStreamPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
+
+        val uri = Uri.parse("https://player.ooyala.com/player/iphone/Y1ZHB1ZDqfhCPjYYRbCEOz0GR8IsVRm1.m3u8")
+
+        ooyalaExoPlayer = ExoStreamPlayer(this)
+        ooyalaExoPlayer?.prepare(uri)
+        exoPlayerView.player = ooyalaExoPlayer?.player
+        ooyalaExoPlayer?.play()
     }
 }
