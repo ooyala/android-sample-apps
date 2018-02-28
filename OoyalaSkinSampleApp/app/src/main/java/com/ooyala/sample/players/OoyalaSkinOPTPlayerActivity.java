9489d9e2a2c6146284f95951f1b6d524537a8344
Akshay Chaudhari <achaudhari@ooyala.com>
2018-02-27 18:20:09 -0800

    [REERT_PLAYER-2639] reverted code for programming screenshot

---
+++ OoyalaSkinSampleApp/app/src/main/java/com/ooyala/sample/players/OoyalaSkinOPTPlayerActivity.java
@@ -0,0 +1,127 @@
+package com.ooyala.sample.players;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.ooyala.android.EmbedTokenGenerator;
+import com.ooyala.android.EmbedTokenGeneratorCallback;
+import com.ooyala.android.EmbeddedSecureURLGenerator;
+import com.ooyala.android.Environment;
+import com.ooyala.android.OoyalaPlayer;
+import com.ooyala.android.PlayerDomain;
+import com.ooyala.android.configuration.Options;
+import com.ooyala.android.skin.OoyalaSkinLayout;
+import com.ooyala.android.skin.OoyalaSkinLayoutController;
+import com.ooyala.android.skin.configuration.SkinOptions;
+import com.ooyala.sample.R;
+
+import org.json.JSONObject;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+
+
+public class OoyalaSkinOPTPlayerActivity extends AbstractHookActivity implements EmbedTokenGenerator {
+
+  @Override
+  void completePlayerSetup(boolean asked) {
+    if(asked) {
+      // Get the SkinLayout from our layout xml
+      OoyalaSkinLayout skinLayout = (OoyalaSkinLayout) findViewById(R.id.ooyalaSkin);
+
+      Environment.AUTHORIZE_HOST = "https://player.ooyala.com";
+      Environment.METADATA_HOST = "https://player.ooyala.com";
+      Environment.CONTENT_TREE_HOST = "https://player.ooyala.com";
+      Environment.BACKLOT_HOST = "https://player.ooyala.com";
+      Environment.DRM_HOST = "https://player.ooyala.com";
+      // Create the OoyalaPlayer, with some built-in UI disabled
+      PlayerDomain domain = new PlayerDomain("http://www.ooyala.com");
+      Options options = new Options.Builder().setShowPromoImage(false).setShowNativeLearnMoreButton(false).setUseExoPlayer(true).build();
+      player = new OoyalaPlayer(pcode, domain, this, options);
+
+      //Create the SkinOptions, and setup React
+      JSONObject overrides = createSkinOverrides();
+      SkinOptions skinOptions = new SkinOptions.Builder().setSkinOverrides(overrides).build();
+      playerLayoutController = new OoyalaSkinLayoutController(getApplication(), skinLayout, player, skinOptions);
+      //Add observer to listen to fullscreen open and close events
+      playerLayoutController.addObserver(this);
+
+      player.addObserver(this);
+      if (player.setEmbedCode(embedCode)) {
+        //Uncomment for autoplay
+        //player.play();
+      } else {
+        Log.e(TAG, "Asset Failure");
+      }
+    }
+  }
+
+  /**
+   * Called when the activity is first created.
+   */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setTitle(getIntent().getExtras().getString("selection_name"));
+    setContentView(R.layout.player_skin_simple_layout);
+    completePlayerSetup(asked);
+
+
+  }
+
+
+
+  /**
+   * Create skin overrides to show up in the skin.
+   * Default commented. Uncomment to show changes to the start screen.
+   * @return the overrides to apply to the skin.json in the assets folder
+   */
+  private JSONObject createSkinOverrides() {
+    JSONObject overrides = new JSONObject();
+//    JSONObject startScreenOverrides = new JSONObject();
+//    JSONObject playIconStyleOverrides = new JSONObject();
+//    try {
+//      playIconStyleOverrides.put("color", "red");
+//      startScreenOverrides.put("playButtonPosition", "bottomLeft");
+//      startScreenOverrides.put("playIconStyle", playIconStyleOverrides);
+//      overrides.put("startScreen", startScreenOverrides);
+//    } catch (Exception e) {
+//      Log.e(TAG, "Exception Thrown", e);
+//    }
+    return overrides;
+  }
+
+  /*
+   * Get the Ooyala Player Token to play the embed code.
+   * This should contact your servers to generate the OPT server-side.
+   * For debugging, you can use Ooyala's EmbeddedSecureURLGenerator to create local embed tokens
+   */
+
+  @Override
+  public void getTokenForEmbedCodes(List<String> embedCodes,
+                                    EmbedTokenGeneratorCallback callback) {
+    String embedCodesString = "";
+    for (String ec : embedCodes) {
+      if (ec.equals("")) embedCodesString += ",";
+      embedCodesString += ec;
+    }
+
+    HashMap<String, String> params = new HashMap<String, String>();
+    params.put("account_id", "");
+
+    /* Uncommenting this will bypass all syndication rules on your asset
+       This will not work unless you have a working API Key and Secret.
+       This is one reason why you shouldn't keep the Secret in your app/source control */
+//     params.put("override_syndication_group", "override_all_synd_groups");
+
+    String uri = "/sas/embed_token/" + pcode + "/" + embedCodesString;
+
+    EmbeddedSecureURLGenerator urlGen = new EmbeddedSecureURLGenerator("F5bGsyOovEON1FJUjhscLRgo3j4a.0ipZk", "ofC7VlnRPNtI7tiOLb5AmOhWrn3KSh_XfdJSInHY");
+
+    URL tokenUrl = urlGen.secureURL("https://player.ooyala.com", uri, params);
+
+    callback.setEmbedToken(tokenUrl.toString());
+  }
+
+}
