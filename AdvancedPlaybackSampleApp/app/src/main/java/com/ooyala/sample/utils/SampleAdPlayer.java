91031caa3188f3c3fd0bb0185f69b70b0d222b2b
Alina Voronkova <alina_voronkova@epam.com>
2018-02-28 14:16:13 +0300

    Revert "[REERT_PLAYER-2639] reverted code for programming screenshot"
    This reverts commit 9489d9e2a2c6146284f95951f1b6d524537a8344.

--- AdvancedPlaybackSampleApp/app/src/main/java/com/ooyala/sample/utils/SampleAdPlayer.java
+++ AdvancedPlaybackSampleApp/app/src/main/java/com/ooyala/sample/utils/SampleAdPlayer.java
@@ -20,6 +20,7 @@ import com.ooyala.android.OoyalaPlayer.State;
 import com.ooyala.android.StateNotifier;
 import com.ooyala.android.player.PlayerInterface;
 import com.ooyala.android.player.PlayerType;
+import com.ooyala.android.player.exoplayer.PlayerBitmapListener;
 import com.ooyala.android.plugin.LifeCycleInterface;
 
 public class SampleAdPlayer extends LinearLayout implements PlayerInterface,
@@ -198,4 +199,8 @@ public class SampleAdPlayer extends LinearLayout implements PlayerInterface,
     return PlayerType.FLAT_PLAYER;
   }
 
+  @Override
+  public void createBitmapScreenshot(PlayerBitmapListener playerBitmapListener) {
+    // TODO
+  }
 }
