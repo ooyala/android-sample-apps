7e02a86be5b6c622736fb0d6aa5bbdad1c69940b
Oscar Robles <orobles@ooyala.com>
2018-07-31 12:28:01 -0500

    Removed unnecessary comment and changed indentation

--- OoyalaSkinSampleApp/app/src/main/java/com/ooyala/sample/players/SsaiPlayerActivity.java
+++ OoyalaSkinSampleApp/app/src/main/java/com/ooyala/sample/players/SsaiPlayerActivity.java
@@ -17,68 +17,69 @@ import org.json.JSONObject;
 
 
 public class SsaiPlayerActivity extends AbstractHookActivity {
-    private OoyalaSsaiManager ssaiManager;
-    private String playerParams = "";
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            playerParams = extras.getString("player_params");
-        }
-        setContentView(R.layout.player_skin_simple_layout);
-        completePlayerSetup(asked);
+  private OoyalaSsaiManager ssaiManager;
+  private String playerParams = "";
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Bundle extras = getIntent().getExtras();
+    if (extras != null) {
+      playerParams = extras.getString("player_params");
     }
+    setContentView(R.layout.player_skin_simple_layout);
+    completePlayerSetup(asked);
+  }
 
-    @Override
-    void completePlayerSetup(boolean asked) {
-        if (asked) {
-            // Get the SkinLayout from our layout xml
-            skinLayout = (OoyalaSkinLayout) findViewById(R.id.ooyalaSkin);
-            // Create the OoyalaPlayer, with some built-in UI disabled
-            PlayerDomain playerDomain = new PlayerDomain(domain);
-            // Create an IQConfiguration class
-            IqConfiguration iqConfiguration = new IqConfiguration.Builder().setPlayerID("SSAI Android Player ID").build();
+  @Override
+  void completePlayerSetup(boolean asked) {
+    if (asked) {
+      // Get the SkinLayout from our layout xml
+      skinLayout = (OoyalaSkinLayout) findViewById(R.id.ooyalaSkin);
+      // Create the OoyalaPlayer, with some built-in UI disabled
+      PlayerDomain playerDomain = new PlayerDomain(domain);
+      // Create an IQConfiguration class
+      IqConfiguration iqConfiguration = new IqConfiguration.Builder().setPlayerID("SSAI Android Player ID").build();
 
-            Options options = new Options.Builder().setIqConfiguration(iqConfiguration).setShowNativeLearnMoreButton(false).setShowPromoImage(false).setUseExoPlayer(true).build();
-            player = new OoyalaPlayer(pcode, playerDomain, options);
-            //Create the SkinOptions, and setup React
-            JSONObject overrides = createSkinOverrides();
-            SkinOptions skinOptions = new SkinOptions.Builder().setSkinOverrides(overrides).build();
-            playerLayoutController = new OoyalaSkinLayoutController(getApplication(), skinLayout, player, skinOptions);
-            //Add observer to listen to fullscreen open and close events
-            playerLayoutController.addObserver(this);
-            player.addObserver(this);
+      Options options = new Options.Builder().setIqConfiguration(iqConfiguration).setShowNativeLearnMoreButton(false).setShowPromoImage(false).setUseExoPlayer(true).build();
+      player = new OoyalaPlayer(pcode, playerDomain, options);
+      //Create the SkinOptions, and setup React
+      JSONObject overrides = createSkinOverrides();
+      SkinOptions skinOptions = new SkinOptions.Builder().setSkinOverrides(overrides).build();
+      playerLayoutController = new OoyalaSkinLayoutController(getApplication(), skinLayout, player, skinOptions);
+      //Add observer to listen to fullscreen open and close events
+      playerLayoutController.addObserver(this);
+      player.addObserver(this);
 
-            ssaiManager = new OoyalaSsaiManager.Builder(player).usePlayerParams(playerParams).build();
+      ssaiManager = new OoyalaSsaiManager.Builder(player).usePlayerParams(playerParams).build();
 
-            if (player.setEmbedCode(embedCode)) {
-            } else {
-                Log.e(TAG, "Asset Failure");
-            }
-        }
+      if (player.setEmbedCode(embedCode)) {
+      } else {
+        Log.e(TAG, "Asset Failure");
+      }
     }
+  }
 
-    /**
-     * Create skin overrides to show up in the skin.
-     * Default commented. Uncomment to show changes to the start screen.
-     * @return the overrides to apply to the skin.json in the assets folder
-     */
-    private JSONObject createSkinOverrides() {
-        JSONObject overrides = new JSONObject();
-        JSONObject adScreenOverrides = new JSONObject();
-        try {
-            adScreenOverrides.put("showControlBar", true);
-            overrides.put("adScreen", adScreenOverrides);
-        } catch (Exception e) {
-            Log.e(TAG, "Exception Thrown", e);
-        }
-        return overrides;
+  /**
+   * Create skin overrides to show up in the skin.
+   *
+   * @return the overrides to apply to the skin.json in the assets folder
+   */
+  private JSONObject createSkinOverrides() {
+    JSONObject overrides = new JSONObject();
+    JSONObject adScreenOverrides = new JSONObject();
+    try {
+      adScreenOverrides.put("showControlBar", true);
+      overrides.put("adScreen", adScreenOverrides);
+    } catch (Exception e) {
+      Log.e(TAG, "Exception Thrown", e);
     }
+    return overrides;
+  }
 
-    @Override
-    protected void onDestroy() {
-        ssaiManager.remove();
-        super.onDestroy();
-    }
+  @Override
+  protected void onDestroy() {
+    ssaiManager.remove();
+    super.onDestroy();
+  }
 }
