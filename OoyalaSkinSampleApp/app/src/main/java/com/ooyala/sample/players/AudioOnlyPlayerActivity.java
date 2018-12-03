cdc3e45f4388aa739ea9a3e69d93c2a0c447e47d
bychkovdmitry <9600921+bychkovdmitry@users.noreply.github.com>
2018-12-03 18:31:58 +0300

    [PLAYER-4624] Update audio only samples (#394)

--- OoyalaSkinSampleApp/app/src/main/java/com/ooyala/sample/players/AudioOnlyPlayerActivity.java
+++ OoyalaSkinSampleApp/app/src/main/java/com/ooyala/sample/players/AudioOnlyPlayerActivity.java
@@ -8,7 +8,6 @@ import com.ooyala.android.configuration.Options;
 import com.ooyala.android.skin.OoyalaSkinLayoutController;
 import com.ooyala.android.skin.configuration.SkinOptions;
 import com.ooyala.sample.R;
-import com.ooyala.sample.utils.AudioOnlyPlayerInfo;
 import org.json.JSONObject;
 
 public class AudioOnlyPlayerActivity extends AbstractHookActivity {
@@ -16,7 +15,7 @@ public class AudioOnlyPlayerActivity extends AbstractHookActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.player_skin_simple_layout);
+    setContentView(R.layout.player_skin_audio_layout);
     completePlayerSetup(asked);
   }
 
@@ -43,20 +42,17 @@ public class AudioOnlyPlayerActivity extends AbstractHookActivity {
   }
 
   private Options createPlayerOptions() {
-    Options.Builder builder = new Options.Builder()
+    return new Options.Builder()
         .setShowNativeLearnMoreButton(false)
         .setShowPromoImage(false)
         .setUseExoPlayer(true)
-        .setPlayerInfo(new AudioOnlyPlayerInfo());
-
-    return builder.build();
+        .setAudioOnly(true) // This is crucial
+        .build();
   }
 
   private JSONObject createSkinOverrides() {
     try {
-      return new JSONObject()
-          .put("upNext", new JSONObject().put("showUpNext", false))
-          .put("endScreen", new JSONObject().put("screenToShowOnEnd", "default"));
+      return new JSONObject();
     } catch (Exception e) {
       throw new RuntimeException("Exception while creating skin overrides", e);
     }
