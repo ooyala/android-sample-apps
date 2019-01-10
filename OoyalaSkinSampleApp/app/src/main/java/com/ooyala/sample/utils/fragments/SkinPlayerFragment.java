10fbfd06290564f31c171263055b25a4f42a9397
isheftel-ooyala <isheftel@ooyala.com>
2019-01-10 18:45:39 +0300

    [PLAYER-4945] Resizable layout logic refactored

--- OoyalaSkinSampleApp/app/src/main/java/com/ooyala/sample/utils/fragments/SkinPlayerFragment.java
+++ OoyalaSkinSampleApp/app/src/main/java/com/ooyala/sample/utils/fragments/SkinPlayerFragment.java
@@ -2,17 +2,11 @@ package com.ooyala.sample.utils.fragments;
 
 import android.graphics.Point;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.*;
+import android.widget.RelativeLayout;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import android.util.Log;
-import android.view.Display;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.RelativeLayout;
-
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.ooyala.android.OoyalaNotification;
 import com.ooyala.android.OoyalaPlayer;
@@ -26,18 +20,18 @@ import com.ooyala.sample.R;
 import java.util.Observable;
 import java.util.Observer;
 
+import static com.ooyala.sample.players.ResizablePlayerActivity.UNSET_ID;
+
 public class SkinPlayerFragment extends Fragment implements Observer, DefaultHardwareBackBtnHandler {
   private static final String EMBED = "JiOTdrdzqAujYa5qvnOxszbrTEuU5HMt";
   final String TAG = this.getClass().toString();
 
-  final String PCODE  = "c0cTkxOqALQviQIGAHWY5hP0q9gU";
+  final String PCODE = "c0cTkxOqALQviQIGAHWY5hP0q9gU";
   final String DOMAIN = "http://ooyala.com";
 
   private OoyalaPlayer player;
   private OoyalaSkinLayout skinLayout;
   OoyalaSkinLayoutController controller;
-
-
 
   @Nullable
   @Override
@@ -45,7 +39,7 @@ public class SkinPlayerFragment extends Fragment implements Observer, DefaultHar
     View view = inflater.inflate(R.layout.configurable_skin_fragment, container, false);
 
     // Get the SkinLayout from our layout xml
-    skinLayout = (OoyalaSkinLayout)view.findViewById(R.id.ooyalaSkin);
+    skinLayout = view.findViewById(R.id.ooyalaSkin);
 
     // Create the OoyalaPlayer, with some built-in UI disabled
     PlayerDomain domain = new PlayerDomain(DOMAIN);
@@ -61,8 +55,7 @@ public class SkinPlayerFragment extends Fragment implements Observer, DefaultHar
     if (player.setEmbedCode(EMBED)) {
       //Uncomment for autoplay
       //player.play();
-    }
-    else {
+    } else {
       Log.e(TAG, "Asset Failure");
     }
     return view;
@@ -72,28 +65,71 @@ public class SkinPlayerFragment extends Fragment implements Observer, DefaultHar
   public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
     inflater.inflate(R.menu.skin_menu, menu);
   }
-  
-  public void resizePlayer(int type) {
+
+  public void resizePlayer(int type, int toolbarHeight, boolean fullscreen) {
     Display display = getActivity().getWindowManager().getDefaultDisplay();
     Point size = new Point();
     display.getSize(size);
     int width = size.x;
     int height = size.y;
 
+    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(width, height);
+
     switch (type) {
       case R.id.action_wide:
-        height = height / 2;
+        updateParamsWide(params, height);
         break;
       case R.id.action_tall:
-        width = width / 2;
+        updateParamsTall(params, width, height, toolbarHeight, fullscreen);
         break;
       case R.id.action_fill:
-        width = ViewGroup.LayoutParams.MATCH_PARENT;
-        height = ViewGroup.LayoutParams.MATCH_PARENT;
+        updateParamsFill(params, height, toolbarHeight, fullscreen);
+        break;
+      case UNSET_ID:
+        updateParamsDefault(params, fullscreen);
+        break;
     }
-    RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(width, height);
-    params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+
     skinLayout.setLayoutParams(params);
+  }
+
+  private void updateParamsWide(RelativeLayout.LayoutParams params, int height){
+    params.height = height / 2;
+    params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+  }
+
+  private void updateParamsTall(RelativeLayout.LayoutParams params, int width, int height, int toolbarHeight, boolean fullscreen) {
+    params.width = width / 2;
+    if (fullscreen) {
+      params.height = ViewGroup.LayoutParams.MATCH_PARENT;
+      params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+    } else {
+      params.height = height - toolbarHeight;
+      params.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+      params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+    }
+  }
+
+  private void updateParamsFill(RelativeLayout.LayoutParams params, int height, int toolbarHeight, boolean fullscreen) {
+    params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+    if (fullscreen) {
+      params.height = ViewGroup.LayoutParams.MATCH_PARENT;
+    } else {
+      params.height = height - toolbarHeight;
+      params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+    }
+    params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+  }
+
+  private void updateParamsDefault(RelativeLayout.LayoutParams params, boolean fullscreen) {
+    if (fullscreen) {
+      params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+      params.height = ViewGroup.LayoutParams.MATCH_PARENT;
+    } else {
+      params.width = skinLayout.getSourceWidth();
+      params.height = skinLayout.getSourceHeight();
+    }
+    params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
   }
 
   @Override
@@ -107,12 +143,12 @@ public class SkinPlayerFragment extends Fragment implements Observer, DefaultHar
       player.suspend();
     }
   }
-  
+
   @Override
   public void onResume() {
     super.onResume();
     if (controller != null) {
-      controller.onResume( getActivity(), this );
+      controller.onResume(getActivity(), this);
     }
     Log.d(TAG, "Player Fragment Restarted");
     if (player != null) {
@@ -130,7 +166,7 @@ public class SkinPlayerFragment extends Fragment implements Observer, DefaultHar
     final String arg1 = OoyalaNotification.getNameOrUnknown(argN);
 
     if (arg1 == OoyalaSkinLayoutController.FULLSCREEN_CHANGED_NOTIFICATION_NAME) {
-      Log.d(TAG, "Fullscreen Notification received : " + arg1 + " - fullScreen: " + ((OoyalaNotification)argN).getData());
+      Log.d(TAG, "Fullscreen Notification received : " + arg1 + " - fullScreen: " + ((OoyalaNotification) argN).getData());
     }
   }
 }
